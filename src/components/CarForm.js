13a60ac63f3cc51a8bcfd5d5983e37085c294eb9
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-11-16 09:33:34 +0100

    in the CarForm component used destructuring of state using useSelector, added handleCostChange

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,13 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
-import { changeName } from "../store"
+import { changeName, changeCost } from "../store"
 
 function CarForm() {
 
     const dispatch = useDispatch();
-    const name = useSelector(state => state.form.name);
+    const { name, cost } = useSelector(state => {
+        return {
+            name: state.form.name,
+            cost: state.form.cost
+        }
+    });
 
     const handleNameChange = (event) => {
         dispatch(changeName(event.target.value));
+    }
+
+    const handleCostChange = (event) => {
+        dispatch(changeCost(parseInt(event.target.value) || 0));
     }
 
     return <div className="car-form panel">
@@ -20,6 +29,16 @@ function CarForm() {
                             className="input is-expended"
                             value={name}
                             onChange={handleNameChange}
+                        />
+                    </div>
+
+                    <div className="field">
+                        <label className="label">Cost</label>
+                        <input
+                            className="input is-expended"
+                            value={cost || ''}
+                            onChange={handleCostChange}
+                            type='number'
                         />
                     </div>
                 </div>
