9ec0a02b9dedfdf4559d3fbdda9a728e7df6594c
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-11-16 11:43:18 +0100

    added renederedCars as mapped cars array to the CarList component

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -7,9 +7,26 @@ function CarList() {
         return state.cars.data;
     });
 
-    console.log(cars);
-    return <div>
-        CarList
+    const handleCarDelete = (car) => {
+        console.log(car);
+    };
+
+    const renderedCars = cars.map(car => {
+        return (
+            <div key={car.id} className='panel'>
+                <p>{car.name} - {car.cost}PLN</p>
+                <button
+                    className='button is-danger'
+                    onCLick={() => handleCarDelete(car)}
+                >Delete
+                </button>
+            </div>
+        )
+    });
+
+    return <div className='car-list'>
+        {renderedCars}
+        <hr />
     </div>
 }
 
